bd2106ede883f1226b46e808b338662518b294f9
jaackies <83699536+jaackies@users.noreply.github.com>
2023-12-03 00:16:36 +0800

    Removed debugging statements

--- aoc 2023/01/d1p1.py
+++ aoc 2023/01/d1p1.py
@@ -1,6 +1,3 @@
-# read input line by line
-# 
-# do the coe\
 import sys
 
 sum = 0
@@ -11,24 +8,13 @@ for line in sys.stdin:
   i = 0
   first = False
   last = False
-  print(line)
   while (not(first and last)):
     if (line[i].isnumeric() and not first):
       sum+= int(line[i])*10
-      print(int(line[i])*10)
       first = True
     if (line[length-1-i].isnumeric() and not last):
       sum+= int(line[length-1-i])
-      print(int(line[length-1-i]))
       last = True
     i+=1
-  print("------")
-  # for i in range(0, length):
-  #   if (line[i].isnumeric() && !first):
-  #     sum += line[i]*10
-  #     first = True
-  #   if(line[length-1-i].isnumeric() && !last):
-  #     sum+= line[length-1-i]
-  #     last = True
-    
+
 print(sum)
